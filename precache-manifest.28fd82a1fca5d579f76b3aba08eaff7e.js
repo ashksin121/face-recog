a8b6e800bccbfd121f9495e9889f767fe0d82858
Ashish Kirti Singh <ashksin121@gmail.com>
2020-01-03 11:03:31 +0530

    Updates

---
+++ precache-manifest.28fd82a1fca5d579f76b3aba08eaff7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b2d6fedc4e1cc63cfb33ba44be57f9",
+    "url": "/face-recog/index.html"
+  },
+  {
+    "revision": "05315c623e464fdf1a8e",
+    "url": "/face-recog/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "16e38467574509f67380",
+    "url": "/face-recog/static/css/main.0ae8ce48.chunk.css"
+  },
+  {
+    "revision": "05315c623e464fdf1a8e",
+    "url": "/face-recog/static/js/2.c0766445.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/face-recog/static/js/2.c0766445.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16e38467574509f67380",
+    "url": "/face-recog/static/js/main.65295924.chunk.js"
+  },
+  {
+    "revision": "10eee49df28bff87b71c",
+    "url": "/face-recog/static/js/runtime-main.812dcc58.js"
+  }
+]);
